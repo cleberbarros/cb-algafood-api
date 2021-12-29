6423e8f0d12744cb7ab1401731c4e6e12885ebe2
cleber <cleber@cleber-G7>
2021-12-29 18:24:20 -0300

    Start Project

---
+++ src/main/java/br/com/cleberbarros/cbalgafoodapi/CbAlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleberbarros.cbalgafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbAlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbAlgafoodApiApplication.class, args);
+	}
+
+}
