808aeb26c6c516fd95155b7b3aa6d423f5862ba8
cleber <cleber@cleber-G7>
2021-12-30 18:39:25 -0300

    3.15. Conhecendo e usando o Lombok

--- src/main/java/br/com/cleberbarros/cbalgafoodapi/domain/model/Cozinha.java
+++ src/main/java/br/com/cleberbarros/cbalgafoodapi/domain/model/Cozinha.java
@@ -2,36 +2,26 @@ package br.com.cleberbarros.cbalgafoodapi.domain.model;
 
 import java.util.Objects;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
+  @EqualsAndHashCode.Include 	//Configurando apenas o atributo Id para o Equals e HashCode
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   
   private String nome;
 
-public Long getId() {
-	return id;
-}
-
-public void setId(Long id) {
-	this.id = id;
-}
-
-public String getNome() {
-	return nome;
-}
-
-public void setNome(String nome) {
-	this.nome = nome;
-}
 
 @Override
 public int hashCode() {
