e9b836ca26482cd9a1fed46ee174c8fca4b594be
cleber <cleber@cleber-G7>
2021-12-31 12:22:52 -0300

    Aulas: 3.18 e 3.19

--- src/main/java/br/com/cleberbarros/cbalgafoodapi/domain/model/Restaurante.java
+++ src/main/java/br/com/cleberbarros/cbalgafoodapi/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -22,12 +23,14 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false) //Caso tenha sido criado as tabelas manualmente, não precisa do nullable=false
 	private String nome;
 	
-	@Column(name = "taxa_frete")
-	private String taxaFrete;
+	@Column(name = "taxa_frete", nullable = false) //nullable = false , estou dizendo que no momento de criação não aceitar nulo
+	private String taxaFrete;						
 	
 	@ManyToOne
+	//@JoinColumn(name = "cozinha_codigo")
 	private Cozinha cozinha;
 	
 }
