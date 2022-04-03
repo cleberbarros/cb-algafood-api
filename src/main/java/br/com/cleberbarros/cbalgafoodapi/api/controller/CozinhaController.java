d3f6d6bcc76c048d671a3fcdfcbd432fb819eca2
cleber <cleber@cleber-G7>
2022-04-02 22:23:10 -0300

    Aula 4.20

--- src/main/java/br/com/cleberbarros/cbalgafoodapi/api/controller/CozinhaController.java
+++ src/main/java/br/com/cleberbarros/cbalgafoodapi/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,8 +41,11 @@ public class CozinhaController {
 	
 	@ResponseStatus(value = HttpStatus.CREATED) //APENAS EXEMPLO, NÃO FAZ SENTINDO RETORNAR STATUS CREATED
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-	  	return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+	  	Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		return ResponseEntity.ok(cozinha);
+		
 	}
 	
 	
